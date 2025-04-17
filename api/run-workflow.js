29a9146671fd208f1d93a105585c88cc2f261a3c
SkeeZT <fayez.fakih@gmail.com>
2025-04-17 17:06:29 +0300

    Update run-workflow.js

--- api/run-workflow.js
+++ api/run-workflow.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://career-builder-two.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
